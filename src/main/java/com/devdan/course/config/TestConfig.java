be33aa33e346cb43567b08ac27572f35e3480c1b
danyllorobemar <danyllo.robemar@gmail.com>
2023-03-13 03:15:16 -0300

    Payment, one-to-one association

--- src/main/java/com/devdan/course/config/TestConfig.java
+++ src/main/java/com/devdan/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devdan.course.entities.Category;
 import com.devdan.course.entities.Order;
 import com.devdan.course.entities.OrderItem;
+import com.devdan.course.entities.Payment;
 import com.devdan.course.entities.Product;
 import com.devdan.course.entities.User;
 import com.devdan.course.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2023-03-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
