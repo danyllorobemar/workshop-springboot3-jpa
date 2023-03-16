157c1c407e251a08f1d9f38739696c9f2249aa93
danyllorobemar <danyllo.robemar@gmail.com>
2023-03-16 00:18:16 -0300

    Exception handling - delete

--- src/main/java/com/devdan/course/services/UserService.java
+++ src/main/java/com/devdan/course/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.devdan.course.entities.User;
 import com.devdan.course.repositories.UserRepository;
+import com.devdan.course.services.exceptions.DatabaseException;
 import com.devdan.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
